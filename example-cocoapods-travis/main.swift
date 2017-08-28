eada511762fc40b5fc2574905ef39e50eac80fd3
Nicholas Ver Voort <nvervoort@blackducksoftware.com>
2017-08-28 14:06:00 -0400

    Initial Commit

---
+++ example-cocoapods-travis/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  example-cocoapods-travis
+//
+//  Created by Nick Ver Voort on 8/28/17.
+//  Copyright © 2017 Black Duck CoPilot. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
