0d96bd7c0a06b4d4a9210711635ee3257bb08089
Nicholas Ver Voort <nvervoort@blackducksoftware.com>
2018-10-11 15:01:02 -0400

    Add test case

---
+++ example-cocoapods-travis-test/example_cocoapods_travis_test.swift
@@ -0,0 +1,17 @@
+//
+//  example_cocoapods_travis_test.swift
+//  example-cocoapods-travis-test
+//
+//  Created by Nick Ver Voort on 10/11/18.
+//  Copyright © 2018 Black Duck CoPilot. All rights reserved.
+//
+
+import XCTest
+
+class example_cocoapods_travis_test: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true);
+    }
+}
